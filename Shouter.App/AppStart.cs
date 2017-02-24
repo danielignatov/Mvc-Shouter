98844ab3f1fd6da5358e2af35181d02031da65a0
Daniel Ignatov <sa3ana3a@abv.bg>
2017-02-24 09:46:50 +0200

    Initial commit.

---
+++ Shouter.App/AppStart.cs
@@ -0,0 +1,14 @@
+﻿namespace Shouter.App
+{
+    using SimpleMVC;
+    using SimpleHttpServer;
+
+    class AppStart
+    {
+        static void Main(string[] args)
+        {
+            HttpServer server = new HttpServer(8081, RouteTable.Routes);
+            MvcEngine.Run(server, "Shouter.App");
+        }
+    }
+}
---
+++ Shouter.App/RouteTable.cs
@@ -0,0 +1,34 @@
+﻿namespace Shouter.App
+{
+    using SimpleHttpServer.Enums;
+    using SimpleHttpServer.Models;
+    using SimpleMVC.Routers;
+    using System.Collections.Generic;
+
+    public static class RouteTable
+    {
+        public static IEnumerable<Route> Routes
+        {
+            get
+            {
+                return new Route[]
+                {
+                    new Route()
+                    {
+                    Name = "Controller/Action/GET",
+                    Method = RequestMethod.GET,
+                    UrlRegex = @"^/(.+)/(.+)",
+                    Callable = new ControllerRouter().Handle
+                },
+                    new Route()
+                    {
+                        Name = "Controller/Action/POST",
+                        Method = RequestMethod.POST,
+                        UrlRegex = @"^/(.+)/(.+)",
+                        Callable = new ControllerRouter().Handle
+                    }
+                };
+            }
+        }
+    }
+}
